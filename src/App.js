cb88127405a4c72b22eaf3aba082c52501920c04
Aakash bhardwaj <officialaakashbhardwaj@gmail.com>
2023-12-04 09:59:57 +0530

    project setup for basic unit tetsing using jest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error,setError]=useState(null)
+  const fetchProduct = async () => {
+    setLoading(true)
+    axios.get(`https://dummyjson.com/products`)
+      .then((res) => setProducts(res.data.products))
+      .catch(err => setError(err))
+    .finally(()=> setLoading(false))
+  }
+  useEffect(() => { fetchProduct() },[])
+  if (loading) return <>Loading...</>
+  if (error) return <>{ error?.message}</>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="cards-container">
+   {products.map(({id,title,description,price,discountPercentage,rating,stock,brand,category,thumbnail,images}) => <div key={id} className='card'>
+     <h3>{title}</h3>
+     <img src={thumbnail} width="150" alt={title } />
+     <p>$ { price}</p>
+     <p>{description}</p>
+     <div className='img-container' >{images.map((image, index) =><img key={index} src={ image} width='50' alt='images' />)}</div>
+   </div>)}
+ </div></div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,64 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/no-wait-for-multiple-assertions */
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
 import App from './App';
+const axios = require("axios");
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+jest.mock('axios');
+
+const mockedProducts = [
+  {
+    id: 1,
+    title: 'Product 1',
+    description: 'Description 1',
+    price: 20,
+    discountPercentage: 10,
+    rating: 4,
+    stock: 100,
+    brand: 'Brand 1',
+    category: 'Category 1',
+    thumbnail: 'thumbnail1.jpg',
+    images: ['image1.jpg', 'image2.jpg'],
+  },
+  // Add more mocked products as needed for testing different scenarios
+];
+
+describe('App Component', () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({ data: { products: mockedProducts } });
+  });
+
+  it('fetches and displays products correctly', async () => {
+    render(<App />);
+
+    // Ensure that the component renders with loading state initially
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    // Wait for the products to be fetched and displayed
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith('https://dummyjson.com/products');
+      expect(screen.getByText('Product 1')).toBeInTheDocument();
+      expect(screen.getByText('$ 20')).toBeInTheDocument();
+      expect(screen.getByText('Description 1')).toBeInTheDocument();
+    });
+  });
+
+  it('displays error message on failed API fetch', async () => {
+    const errorMessage = 'Failed to fetch products';
+    axios.get.mockRejectedValue(new Error(errorMessage));
+
+    render(<App />);
+
+    // Ensure that the component renders with loading state initially
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    // Wait for the error message to be displayed
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith('https://dummyjson.com/products');
+      expect(screen.getByText(errorMessage)).toBeInTheDocument();
+    });
+  });
+
+  // Add more test cases as needed to cover other component functionalities
 });
